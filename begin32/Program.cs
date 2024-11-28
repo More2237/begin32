8d6733bb501958b819d5b4bd7f95654f4ff05b3e
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-28 18:17:20 +0500

    djhf

---
+++ begin32/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace begin32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tf = int.Parse(Console.ReadLine());
+            
+
+            var resulttc = (tf - 32) * 5 / 9;
+            Console.WriteLine(resulttc);
+        }
+    }
+}
